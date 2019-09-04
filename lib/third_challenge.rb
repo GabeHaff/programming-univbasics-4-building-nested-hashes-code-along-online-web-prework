b00e19a2d54f8654cafc525c08d9dfec75483e62
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-09-04 16:59:31 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -8,16 +8,16 @@ def third_challenge
         :name => "Lady Montague", :age => "54"
         },
       :hero => {
-        
+        :name =>"Romeo", :age => "15" , :status => "alive"
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        :name => "Lord Capulet" , :age => "50"
         },
       :matriarch => {
-        #your key/value pairs here
+      
         },
       :heroine => {
         #your key/value pairs here
